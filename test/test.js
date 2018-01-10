e79a7ad89a430e049ca82fd7d172435d139b3c7e
Kennedy Rose <kennedy@kennedyrose.com>
2018-01-10 17:04:48 -0600

    Encrypt test

--- test/test.js
+++ test/test.js
@@ -1,5 +1,5 @@
 import { expect } from 'chai'
-import { outputFile, remove } from 'fs-extra'
+import { readFile, outputFile, remove } from 'fs-extra'
 import { getKey, encrypt } from '../src'
 
 process.env.ENVDOTJS_KEY = '123'
@@ -25,6 +25,9 @@ describe('encrypt', () => {
 	it('Encrypts a file', async () => {
 		await outputFile('./env.js', `module.exports = { TEST_ENV: 'test value' }`)
 		await encrypt()
+		let contents = await readFile('./env.js.enc')
+		contents = contents.toString()
+		expect(contents).to.equal('95c1fe2c574151be5afd5bc1cb028b2e199e7f8fa70605cd227ae873a0d23479b7d1e732267ad0024fe69b')
 		await remove('./env.js')
 		await remove('./env.js.enc')
 	})
