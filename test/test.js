5f340ac42929d497083dbd49bdebf6ed432bfc09
Kennedy Rose <kennedy@kennedyrose.com>
2018-01-10 17:01:26 -0600

    getKey tests

--- test/test.js
+++ test/test.js
@@ -1,10 +1,31 @@
 import { expect } from 'chai'
-import theModule from '../src'
+import { outputFile, remove } from 'fs-extra'
+import { getKey, encrypt } from '../src'
 
-describe('Default module', () => {
-	it('Should have content', () => {
-		const testVar = theModule()
-		expect(testVar).to.not.be.empty
-		expect(testVar.test).to.equal('123')
+process.env.ENVDOTJS_KEY = '123'
+
+describe('getKey', () => {
+	it('Reads the environment key from a variable', () => {
+		const key = getKey({ key: '123' })
+		expect(key).to.equal('123')
+	})
+	it('Reads the environment key from a file', async () => {
+		await outputFile('./envdotjs-key', '123')
+		const key = getKey()
+		expect(key).to.equal('123')
+		await remove('./envdotjs-key')
+	})
+	it('Reads the environment key from the environment', () => {
+		const key = getKey()
+		expect(key).to.equal('123')
+	})
+})
+
+describe('encrypt', () => {
+	it('Encrypts a file', async () => {
+		await outputFile('./env.js', `module.exports = { TEST_ENV: 'test value' }`)
+		await encrypt()
+		await remove('./env.js')
+		await remove('./env.js.enc')
 	})
 })
