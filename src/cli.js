35d926365f4b655484c043683be95a6023bcc282
Kennedy Rose <kennedy@kennedyrose.com>
2018-01-12 11:48:04 -0600

    Added unencrypt to cli

--- src/cli.js
+++ src/cli.js
@@ -1,15 +1,16 @@
 #! /usr/bin/env node
 import meow from 'meow'
-import { encrypt } from './index'
+import { encrypt, unencrypt } from './index'
 
 const cli = meow(`
     Usage
       $ envdotjs
 
     Options
-      --key, -k     Set an encryption key, defaults to contents of envdotjs-key file in root
-      --path, -p    Path to your .js file, defaults to env.js
-      --output, -o  Path to the encrypted output, defaults to {path}.enc
+      --key, -k        Set an encryption key, defaults to contents of envdotjs-key file in root
+      --path, -p       Path to your .js file, defaults to env.js
+		--output, -o     Path to the encrypted output, defaults to {path}.enc
+		--unencrypt, -u  Unencrypts a file, defaults to env.js.enc
 
     Examples
       $ envdotjs --key my-key
@@ -17,17 +18,26 @@ const cli = meow(`
 	flags: {
 		key: {
 			type: 'string',
-			alias: 'k'
+			alias: 'k',
 		},
 		path: {
 			type: 'string',
-			alias: 'p'
+			alias: 'p',
 		},
 		output: {
 			type: 'string',
-			alias: 'o'
+			alias: 'o',
 		},
+		unencrypt: {
+			type: 'string',
+			alias: 'u',
+		}
 	}
 })
 
-encrypt(cli.flags)
+if (!cli.flags.unencrypt) {
+	encrypt(cli.flags)
+}
+else{
+	unencrypt(cli.flags)
+}
