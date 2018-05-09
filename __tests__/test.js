8fe3ec08eccdfcf7f5277ce83ea1f315e13bb623
Kennedy Rose <kennedy@kennedyrose.com>
2018-05-09 08:40:30 -0500

    esca-scripts

--- __tests__/test.js
+++ __tests__/test.js
@@ -1,23 +1,22 @@
-import { expect } from 'chai'
 import { readFile, outputFile, remove } from 'fs-extra'
-import { getKey, encrypt, unencrypt, load } from '../dist-es5'
+import { getKey, encrypt, unencrypt, load } from '../dist'
 
 process.env.ENVDOTJS_KEY = '123'
 
 describe('getKey', () => {
 	it('Reads the environment key from a variable', () => {
 		const key = getKey({ key: '123' })
-		expect(key).to.equal('123')
+		expect(key).toEqual('123')
 	})
 	it('Reads the environment key from a file', async () => {
 		await outputFile('./envdotjs-key', '123')
 		const key = getKey()
-		expect(key).to.equal('123')
+		expect(key).toEqual('123')
 		await remove('./envdotjs-key')
 	})
 	it('Reads the environment key from the environment', () => {
 		const key = getKey()
-		expect(key).to.equal('123')
+		expect(key).toEqual('123')
 	})
 })
 
@@ -27,7 +26,7 @@ describe('encrypt', () => {
 		await encrypt()
 		let contents = await readFile('./env.js.enc')
 		contents = contents.toString()
-		expect(contents).to.equal('95c1fe2c574151be5afd5bc1cb028b2e199e7f8fa70605cd227ae873a0d23479b7d1e732267ad0024fe69b')
+		expect(contents).toEqual('95c1fe2c574151be5afd5bc1cb028b2e199e7f8fa70605cd227ae873a0d23479b7d1e732267ad0024fe69b')
 		await remove('./env.js')
 		await remove('./env.js.enc')
 	})
@@ -37,7 +36,7 @@ describe('unencrypt', () => {
 	it('Unencrypts a file', async () => {
 		await outputFile('./env.js.enc', '95c1fe2c574151be5afd5bc1cb028b2e199e7f8fa70605cd227ae873a0d23479b7d1e732267ad0024fe69b')
 		const contents = unencrypt()
-		expect(contents.TEST_ENV).to.equal('test value')
+		expect(contents.TEST_ENV).toEqual('test value')
 		await remove('./env.js.enc')
 	})
 })
@@ -46,7 +45,7 @@ describe('load', () => {
 	it('Loads an environment file', async () => {
 		await outputFile('./env.js.enc', '95c1fe2c574151be5afd5bc1cb028b2e199e7f8fa70605cd227ae873a0d23479b7d1e732267ad0024fe69b')
 		load()
-		expect(process.env.TEST_ENV).to.equal('test value')
+		expect(process.env.TEST_ENV).toEqual('test value')
 		await remove('./env.js.enc')
 	})
 })
