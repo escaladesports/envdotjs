ef53c1be066ff13f2ab734e8ae186b8ff1a0af4e
Kennedy Rose <kennedy@kennedyrose.com>
2018-05-29 15:19:01 -0500

    Separated commands

--- src/cli.js
+++ src/cli.js
@@ -7,22 +7,23 @@ sc.command(`version`, {
 		desc: `Display version`,
 		callback: () => console.log(version),
 	})
-	.command(`encrypt`, {
+
+sc.command(`encrypt`, {
 		description: `Encrypts an env.js file`,
 		callback: encrypt,
 	})
-		.option(`key`, {
-			abbr: `K`,
-			desc: `Set an encryption key, defaults to contents of envdotjs-key file in root`,
-		})
-		.option(`path`, {
-			abbr: `P`,
-			desc: `Path to your .js file, defaults to env.js`,
-		})
-		.option(`output`, {
-			abbr: `O`,
-			desc: `Path to the encrypted output, defaults to {path}.enc`,
-		})
+	.option(`key`, {
+		abbr: `K`,
+		desc: `Set an encryption key, defaults to contents of envdotjs-key file in root`,
+	})
+	.option(`path`, {
+		abbr: `P`,
+		desc: `Path to your .js file, defaults to env.js`,
+	})
+	.option(`output`, {
+		abbr: `O`,
+		desc: `Path to the encrypted output, defaults to {path}.enc`,
+	})
 
 
 sc.parse()
